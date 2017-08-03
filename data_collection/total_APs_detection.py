7da7cc930554db16eee1f36e0af3c2cac017ec77
Jeff Wang <wrh003769@163.com>
2017-08-03 21:00:20 +0800

    Detect the number of APs

---
+++ data_collection/total_APs_detection.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+dataset = pd.read_csv("test_info(1).csv", header=0)
+total_APs = list(dataset.ix[:,5])
+total_APs = list(set(total_APs))
+
+print(len(total_APs))
+
+dic ={}
+
+for i in range(len(total_APs)):
+    dic[total_APs[i]] = i
+
+for key in sorted(dic.keys()):
+    print (key, '\t', dic[key])
