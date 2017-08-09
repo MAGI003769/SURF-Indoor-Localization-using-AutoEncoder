cbb96f63dc8bb9fc23664a39171d84be4d829c89
Jeff Wang <wrh003769@163.com>
2017-08-09 23:04:53 +0800

    Constructing database
    Totally detect 200 APs

--- data_collection/total_APs_detection.py
+++ data_collection/total_APs_detection.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 
-dataset = pd.read_csv("test_info_data.csv", header=0)
-total_APs = list(dataset.ix[:,5])
+dataset = pd.read_csv("userinput.csv", header=0)
+total_APs = list(dataset.ix[:,4])
 total_APs = list(set(total_APs))
 
 print(len(total_APs))
@@ -12,5 +12,19 @@ dic ={}
 for i in range(len(total_APs)):
     dic[total_APs[i]] = i
 
+
 for key in sorted(dic.keys()):
     print (key, '\t', dic[key])
+
+mapping = pd.DataFrame.from_dict(dic, 'index')
+mapping.to_csv('mapping.csv', header=0)
+
+
+
+dic2 = {}
+mapping = pd.read_csv("mapping.csv", header=0)
+print(dataset.ix[:,:].shape)
+for i in range(dataset.ix[:, 0].shape[0]):
+	dic2.update({dataset.ix[i, 0]: i})
+print(dic2)
+print('Fin')
